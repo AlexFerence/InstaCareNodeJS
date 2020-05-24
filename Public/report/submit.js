314152c9a23d7216c70a0952076905d31f8ccc42
Param Thakkar <pht0104@gmail.com>
2020-05-24 13:16:17 -0400

    bearer

--- Public/report/submit.js
+++ Public/report/submit.js
@@ -3,7 +3,7 @@ function updateSymptoms() {
     var url = "https://instachat-openhack.herokuapp.com/patient";
     xhr.open("PATCH", url, true);
     xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.setRequestHeader("Authorization","BEARER " + localStorage.getItem("token"));
+    xhr.setRequestHeader("Authorization","Bearer " + localStorage.getItem("token"));
 
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4 && xhr.status == 200) {
