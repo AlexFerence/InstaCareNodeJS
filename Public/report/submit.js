c71fb69f5179ace5cf52d3bcdd661b305c480ab9
Param Thakkar <pht0104@gmail.com>
2020-05-24 12:48:40 -0400

    Create submit.js

---
+++ Public/report/submit.js
@@ -0,0 +1,19 @@
+function updateSymptoms() {
+    var xhr = new XMLHttpRequest();
+    var url = "https://instachat-openhack.herokuapp.com/patient";
+    xhr.open("PATCH", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("Authorization","BEARER " + localStorage.getItem("token"));
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var json = JSON.parse(xhr.responseText);
+        }
+    };
+
+    var data = JSON.stringify({
+        "symptoms": document.getElementById("symptoms").value
+    });
+
+    xhr.send(data);
+}
